aad721348b4a978405e5b80ed105198fef51aee4
Charles Langlois <contact@charleslanglois.dev>
2026-04-25 03:37:19 -0400

    project files

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    let
+      out = system:
+        let pkgs = nixpkgs.legacyPackages."${system}";
+        in {
+
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              (python314.withPackages (ps: with ps; [ pip uv ty pytest ]))
+              ruff
+            ];
+          };
+
+          # defaultPackage = with pkgs.poetry2nix; mkPoetryApplication {
+          #     projectDir = ./.;
+          #     preferWheels = true;
+          # };
+
+          # defaultApp = utils.lib.mkApp {
+          #     drv = self.defaultPackage."${system}";
+          # };
+
+        };
+    in with utils.lib; eachSystem defaultSystems out;
+
+}
